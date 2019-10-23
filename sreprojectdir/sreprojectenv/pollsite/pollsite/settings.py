3f3f1f87472e0948a87f73ab4d752088131b66d8
Carissa Allen <carissawood@outlook.com>
2019-10-22 20:53:46 -0700

    Update database settings for Cloud SQL

--- sreprojectdir/sreprojectenv/pollsite/pollsite/settings.py
+++ sreprojectdir/sreprojectenv/pollsite/pollsite/settings.py
@@ -77,7 +77,10 @@ WSGI_APPLICATION = 'pollsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'HOST': 'cs410-site-reliability-eng:us-west1:polls-instance',
+        'NAME': 'polls',
+        'USER': 'admin',
+        'PASSWORD': 'MegaPassword',
     }
 }
 
