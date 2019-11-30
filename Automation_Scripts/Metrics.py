3a31dc662e390d375c33c3b7bf8faf5498d2030e
root <root@sweet-canary-testing.us-west1-b.c.sreproject.internal>
2019-11-30 02:13:52 +0000

    added metrics per week

--- Automation_Scripts/Metrics.py
+++ Automation_Scripts/Metrics.py
@@ -13,7 +13,7 @@ def get_log_names(dir):
     files = {} 
     files['names'] = glob(os.path.join(dir, 'access*'))
     files['dates'] = [datetime.datetime.fromtimestamp(os.stat(file).st_mtime) for file in files['names']]
-    files['week'] = [int(date.isocalendar()[1])-39 for date in files['dates']]
+    files['week'] = [str(int(date.isocalendar()[1])-39) for date in files['dates']]
     files['dates'] = [date.strftime("%d-%b-%Y") for date in files['dates']]
     return files
 
@@ -35,9 +35,8 @@ def codes_per_date(files):
 
     codes = {}
 
-    for date, file in zip(files['dates'], files['names']):
+    for date, file, week in zip(files['dates'], files['names'], files['week']):
         print('opening file', file)
-        codes[date] = {}
         if pathlib.Path(file).suffix == '.gz':
             with gzip.open(file, 'rb') as f:
                 codes[date] = extract_codes(f)
@@ -46,17 +45,41 @@ def codes_per_date(files):
             with open(file, 'r') as f:
                 codes[date] = extract_codes(f)
 
+        codes[date].update({'week':week})
+
     return codes
 
-                
+
+def codes_per_week(codes_daily):
+    
+    codes = {}
+
+    for date in codes_daily:
+        week = codes_daily[date]['week']
+        if week not in codes:
+            codes[week] = codes_daily[date] 
+            codes[week].pop('week', None)
+        else:
+            for key in codes_daily[date]:
+                if key in codes[week] and key != 'week':
+                    codes[week][key] += codes_daily[date][key]
+                elif key != 'week':
+                    codes[week][key] = codes_daily[date][key]
+
+    return codes
+
+
 def main():
     log_file_dir = '/var/log/nginx'
     files = get_log_names(log_file_dir)
 
-    codes = codes_per_date(files)    
+    codes_daily = codes_per_date(files)    
 
-    print(codes)
+    codes_weekly = codes_per_week(codes_daily)
+
     print(files)
+    print(codes_daily)
+    print(codes_weekly)
 
 
 if __name__ == '__main__':
