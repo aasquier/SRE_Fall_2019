d772a5a7bbe3ae1ad5994f75a58aa05cb6f12d1e
root <root@sweet-canary-testing.us-west1-b.c.sreproject.internal>
2019-12-05 19:28:29 +0000

    Added canary test to polls

--- polls/views.py
+++ polls/views.py
@@ -38,6 +38,7 @@ class IndexView(generic.ListView):
         show_feature = ldclient.get().variation("poll-1", user, False)
         if not show_feature:
             return None
+        ld_client.get().close()
         """
         Return the last five published questions (not including those set to be
         published in the future).
